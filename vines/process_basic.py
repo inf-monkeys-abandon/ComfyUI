dbc77fbe10b8818f2ff3b325f2701e4cfbbd09e6
cj <changjiangthecoder@gmail.com>
2024-02-27 21:02:36 +0700

    update

--- vines/process_basic.py
+++ vines/process_basic.py
@@ -4,11 +4,50 @@ import time
 import pathlib
 import uuid
 
+import websocket
 from bullmq.job import Job
 import api
 import config
+import json
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
+server_address = '127.0.0.1:8188'
+client_id = str(uuid.uuid4())
+ws = websocket.WebSocket()
+ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+
+
+def track_progress(prompt, prompt_id):
+    node_ids = list(prompt.keys())
+    finished_nodes = []
+
+    while True:
+        out = ws.recv()
+        if isinstance(out, str):
+            message = json.loads(out)
+            print("receive message: ")
+            if message['type'] == 'progress':
+                data = message['data']
+                current_step = data['value']
+                print('In K-Sampler -> Step: ', current_step, ' of: ', data['max'])
+            if message['type'] == 'execution_cached':
+                data = message['data']
+                for itm in data['nodes']:
+                    if itm not in finished_nodes:
+                        finished_nodes.append(itm)
+                        print('Progess: ', len(finished_nodes), '/', len(node_ids), ' Tasks done')
+            if message['type'] == 'executing':
+                data = message['data']
+                if data['node'] not in finished_nodes:
+                    finished_nodes.append(data['node'])
+                    print('Progess: ', len(finished_nodes), '/', len(node_ids), ' Tasks done')
+
+                if data['node'] is None and data['prompt_id'] == prompt_id:
+                    break  # Execution is done
+        else:
+            continue
+    return
 
 
 def process(job: Job):
@@ -41,15 +80,16 @@ def process(job: Job):
 
     # 轮询结果
     logging.info('Polling result')
-    status = ""
-    timeout_time = time.time() + 60 * 5
-    while status != "success":
-        res = api.get_task(prompt_id)
-        if res:
-            break
-        if time.time() > timeout_time:
-            raise Exception("运行 ComfyUI 超时")
-        time.sleep(1)
+    # status = ""
+    # timeout_time = time.time() + 60 * 5
+    # while status != "success":
+    #     res = api.get_task(prompt_id)
+    #     if res:
+    #         break
+    #     if time.time() > timeout_time:
+    #         raise Exception("运行 ComfyUI 超时")
+    #     time.sleep(1)
+    track_progress(prompt=request['prompt'], prompt_id=prompt_id)
 
     # 上传图片
     hrefs = []
