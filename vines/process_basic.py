fa9d6e5d911292a7f17742c842698c59bda24dac
cj <changjiangthecoder@gmail.com>
2024-02-28 16:21:16 +0700

    update

--- vines/process_basic.py
+++ vines/process_basic.py
@@ -157,13 +157,15 @@ def process_prompt(job: Job):
 
 
 def process_install_dependency(job: Job):
-    data = job.data.get('data')
-    type = data.get('type')
-    dependency = data.get('data')
-    print(f"Install Dependency: {type} {dependency}", )
-    if type == 'NODE':
-        install_custom_node(dependency)
-    elif type == 'MODEL':
-        install_model(dependency)
-
-    return {"success": True}
+    print(job.data)
+    pass
+    # data = job.data.get('data')
+    # type = data.get('type')
+    # dependency = data.get('data')
+    # print(f"Install Dependency: {type} {dependency}", )
+    # if type == 'NODE':
+    #     install_custom_node(dependency)
+    # elif type == 'MODEL':
+    #     install_model(dependency)
+    #
+    # return {"success": True}
